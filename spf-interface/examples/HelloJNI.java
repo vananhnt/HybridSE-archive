8b18497f071eeb47762fd65156c682abeb5d11f9
vananhnt <vananhnt97@gmail.com>
2022-06-01 13:07:18 +0900

    adding samples

---
+++ spf-interface/examples/HelloJNI.java
@@ -0,0 +1,105 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class HelloJNI {
+	static {
+        System.loadLibrary("hello");    // loads libhello.so
+    }
+
+    private native String addStr(String str);    
+
+    private native int print(String name);
+    
+    private native int incr(int x, String pw);
+
+	private native float findSqrt(int number);
+
+	private native int calPerimeter(int a, int b, int c);
+
+    private native String getHostName();
+    private native int sockconnect(String arg);
+
+    public double area(int a, int b, int c) {
+    	int p;
+    	double area = 0;
+    	double s = 0;
+		p = calPerimeter(a, b, c);
+		if (p > 0) {
+			s = p/2;
+			area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+		}
+		return area;
+	}
+
+    public int foo(String s) {
+	int len = s.length();
+	int res = 0;
+	if (len > 0) {
+		s = addStr(s);
+	}
+	return s.length();
+	}
+
+    public int test(int x, int y) {
+    	int tmp = 2*y;
+    	if (x >= 0 && x > y && tmp > x+2) {
+			x = incr(x, "messesage");
+		}
+		if (x + y < 10) {
+			x = x + y;
+		}
+		return x;
+		}
+
+		public void sender() {
+			try {
+				String msg = "GET / HTTP/1.1\\r\\n\\r\\n";
+				if (sockconnect(msg) == 0) {
+					System.out.println("Message sent");
+				} else {
+					System.out.println("Connecion error");
+				}
+
+			} catch (Exception e) {
+				System.out.println("Exception in native code");
+			}
+		}
+
+//		public void connect() {
+//			try {
+//				final String HOST = getHostName();
+//				final int PORT = 1024;
+//
+//				Socket socket = new Socket(HOST, PORT);
+//				OutputStream socketOutput = socket.getOutputStream();
+//				InputStream socketInput = socket.getInputStream();
+//
+//				socketOutput.write(5);
+//				int i = socketInput.read();
+//				while (i != -1) {
+//					System.out.print(i);
+//					i = socketInput.read();
+//				}
+//				System.out.println();
+//
+//				socket.close();
+//			}
+//			catch (UnknownHostException e) {
+//				System.out.println("Host is unknown");
+//			}
+//			catch (IOException e) {
+//				System.out.println("I/O error occured when creating the socket");
+//			}
+//		}
+
+    public static void main(String[] args) {
+		(new HelloJNI()).sender();
+    	//(new HelloJNI()).area(4, 3, 5);
+		//System.out.println((new HelloJNI()).foo("Hello mesg"));
+    }
+}
+
+
